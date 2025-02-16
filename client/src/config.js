9ce697c0e5818c14e24d42caea82f526b19649aa
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-02-17 00:31:47 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // config.js
- const API_BASE_URL = "https://project-backend.vercel.app"; 
+ const API_BASE_URL = "https://project-backend-git-main-anurag-sens-projects-de1ca9a8.vercel.app"; 
 
 export default API_BASE_URL;
