1d7e4512bc6b7b4b6b522a2e21ce52461e434d8b
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-02-16 23:58:06 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_BASE_URL = "https://project-backend-one-xi.vercel.app/"; // Replace with your actual backend URL
+ const API_BASE_URL = "https://project-backend.vercel.app"; 
 
 export default API_BASE_URL;
